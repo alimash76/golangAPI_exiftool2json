2f871fc2972dc0d69a5a999b4bd171153ee21be0
Ali Hejazi <alihejazi76@gmail.com>
2021-10-17 15:16:30 +0330

    XML is now Unmarshaled

--- main.go
+++ main.go
@@ -3,36 +3,43 @@ package main
 import (
 	"os/exec"
 	"fmt"
+	// "encoding/json"
+	"encoding/xml"
 	// "strconv"
 )
 
+// XML structs:
+type IndexType struct{
+	IndexStruct []TableStruct	`xml:"table"`
+}
+
+type TableStruct struct{
+	Desc		string		`xml:"desc"`
+	Tags		[]TagStruct	`xml:"tag"`
+}
+
+type TagStruct struct{
+	Descs	[]string	`xml:"desc"`
+}
+
+
 func main(){
-	// out, cmd := exec.Command("./command.sh").Output()
-	// err := cmd.Start()
-	// if err != nil{
-	// 	fmt.Printf("Error!")
-	// }
-	// fmt.Printf("wating for the command to finish...")
-	// err = cmd.Wait()
-	// fmt.Printf("Command finished:\n%s", out)
 
-
-	// cmd := exec.Command("sleep", "5")
-	// err := cmd.Start()
-	// if err != nil {
-	// 	fmt.Printf("Error")
-	// }
-	// fmt.Printf("Waiting for command to finish...")
-	// err = cmd.Wait()
-	// fmt.Printf("Command finished with error: %v", err)
-
-	cmd := exec.Command("./command.sh")
+	//Execute the Command and get the output
+	cmd := exec.Command("./runexiftool.sh")
 	stdout, err := cmd.CombinedOutput()
 	if err != nil{
 		fmt.Printf("Error")
 	}
-	convStd := string(stdout[:])
-	fmt.Printf(convStd)
+
+	//Create xml struct
+	data := string(stdout)
+	var s IndexType
+	xml.Unmarshal([]byte(data), &s)
+
+	// fmt.Println(s.IndexStruct)
+	// fmt.Println(s)
 	
+	//Json
 }
 
